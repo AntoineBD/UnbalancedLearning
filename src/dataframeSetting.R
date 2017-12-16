a37bbc4078a8d8b50e761d5f8e8a2672e3c9f31b
Eric <eric.hammel01@laposte.net>
2017-12-16 12:41:40 +0100

    adding file and function header

--- src/dataframeSetting.R
+++ src/dataframeSetting.R
@@ -1,8 +1,17 @@
+## Fichier: dataframeSetting.r
+## Auteur : Eric HAMMEL
+## Description : Importation du jeu de données et 
+##               modifications nécessaires pour les diverses manipulations
+## Date : 17 décembre 2018
 
-library("data.table") # library adapting dataframes for large datasets
-
+## Importation du jeu de données kddcup en utilisant la librairie data.table qui 
+## est adpatée à un fichier de cette taille
+## INPUT : pas d'INPUT
+## OUTPUT :
+##  le jeu de données de type data.table
 createDataset <- function()
 {
+  library("data.table") # library adapting dataframes for large datasets
   # setting up header
   dtHeader <- read.table("data/kddcup.header.txt", sep = ",", header = TRUE)
   
@@ -12,6 +21,5 @@ createDataset <- function()
                           stringsAsFactors = TRUE)
   # removing the point from attack_type factor
   kddcup.dataset$attack_type <- as.factor(gsub(".$","",kddcup.dataset$attack_type))
-  
   return(kddcup.dataset)
   }
